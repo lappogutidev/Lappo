b8882e65f8feb61e077201c5c41251767bb6c864
lappogutidev <lappoguti00@gmail.com>
2018-06-13 15:17:15 -0400

    Initial Commit

---
+++ src/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name) {
+
+        this.name = name;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public int getAge() {
+
+        return age;
+
+    }
+
+}
